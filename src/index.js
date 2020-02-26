64103df67215646a78ca58e8039b6faf69562f00
Steven Zhu <stevenzhu03@gmail.com>
2020-02-26 10:41:54 -0500

    deliverables complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,94 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4674 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+
+  fetch(imageURL)
+  .then(resp => resp.json())
+  .then (image => {
+    let picture = document.getElementById('image')
+    let name = document.getElementById('name')
+    let likes = document.getElementById('likes')
+    let ul = document.getElementById('comments')
+
+    picture.src = image.url
+    name.innerText = image.name
+    likes.innerText = image['like_count']
+
+    image.comments.forEach(comment => {
+      let li = document.createElement('li')
+      li.id = comment.id
+
+      li.innerHTML = `
+      ${comment.content} <button class="delete"> Delete </delete>
+      `
+
+      ul.append(li)
+    }
+    )
+
+  })
+
+  document.addEventListener('click', event => {
+    if (event.target.id === "like_button"){
+      let likes = document.getElementById('likes')
+      likes.innerText++
+
+      fetch(likeURL, {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          image_id: 4674
+        })
+      })
+      .then(resp => resp.json)
+      .then(like => console.log(like))
+    }
+
+    if (event.target.className === "delete"){
+      fetch(commentsURL + `+${event.target.parentElement.id}`, {
+        method: "DELETE",
+      })
+      .then(resp => resp.json)
+      .then(event.target.parentNode.remove())
+    }
+  })
+
+  document.addEventListener('submit', event =>{
+    event.preventDefault()
+    let comment = event.target.comment.value
+    let ul = document.getElementById('comments')
+
+    let li = document.createElement('li')
+    li.innerText = comment
+
+    ul.append(li)
+  
+    fetch(commentsURL, {
+      method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          image_id: 4674,
+          content: comment
+        })
+    })
+    .then(resp => resp.json)
+    .then(comment => console.log(comment))
+    document.getElementById("comment_form").reset()
+  })
+
 
 })
