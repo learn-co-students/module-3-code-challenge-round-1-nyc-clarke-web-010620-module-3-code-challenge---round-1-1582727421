a45b1e1fa353737f1931fcafbab8393d130d79c7
jhuang429 <jhuang9240@gmail.com>
2020-02-26 10:29:56 -0500

    deliverables complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,117 @@
+let imageId = 4667; //Enter the id from the fetched image here
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`;
+const likeURL = `https://randopic.herokuapp.com/likes/`;
+const commentsURL = `https://randopic.herokuapp.com/comments/`;
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+  // console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  fetch(imageURL).then(resp=> resp.json()).then(
+    image => {
+      loadcontent(image);
+  });
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+  document.addEventListener("click",e => {
+    switch(true){
+      case(e.target.id === "like_button"):
+        addLike()
+        break;
+      case(e.target.id === "delete_button"):
+          e.preventDefault()
+          let id = e.target.parentNode.dataset.id
+          deleteComment(id)
+          e.target.parentNode.remove()
+        break;
+      case(e.target.type === "submit"):
+        e.preventDefault()
+        createComment()
+        break;
+    };
+    
+  })//end of click listener
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+})  //end of DOMContentLoaded listener
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-})
+
+
+//helper functions below:
+
+
+function loadcontent(imageJSON){
+  let img = document.querySelector("img");
+  img.src = imageJSON.url;
+
+  let title = document.getElementById("name");
+  title.innerText = imageJSON.name;
+
+  let likes = document.getElementById("likes");
+  likes.innerText = "Likes: " + imageJSON.like_count;
+
+  let comments = imageJSON.comments;
+  let ul = document.getElementById("comments");
+  for (i=0; i < comments.length; i++){
+    loadComments(comments[i], ul);
+  };
+
+}
+
+
+function loadComments(comment, tag){
+  let li = document.createElement("li");
+  li.innerText = comment.content ;
+  li.dataset.id = comment.id
+  button = createButton()
+  li.append(button)
+  tag.append(li);
+}
+ 
+ function addLike(){
+  let likes = document.getElementById("likes");
+  let num = parseInt(likes.innerText.split(" ")[1])
+  num++
+  likes.innerText = "Likes: " + num;
+
+  fetch(likeURL, {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({image_id: imageId})
+  })
+
+}
+ 
+ function createComment(){
+  let commentbox = document.getElementById("comment_input");
+  let words = commentbox.value
+
+  fetch(commentsURL, {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      image_id: imageId,
+      content: words
+    })
+  }).then(resp => resp.json()).then(comment =>{
+    let ul = document.getElementById("comments")
+    loadComments(comment, ul)
+  })
+
+}
+
+function createButton(){
+  let button = document.createElement("button")
+  button.id = "delete_button"
+  button.innerText ="Delete"
+  return button 
+}
+
+function deleteComment(id) {
+  fetch(`${commentsURL}/${id}`, {
+    method: "DELETE"})
+}
