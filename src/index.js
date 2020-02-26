456615eebb6f4122d7ba3dcf9e1fcec0009700ff
enteraether <thetheresagarcia@gmail.com>
2020-02-26 11:01:49 -0500

    ...

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4668
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +11,72 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+
+
+  loadImage()
+
+
+
+function loadImage(){
+  return fetch(imageURL)
+  .then(resp=>resp.json())
+  .then(image=>renderImage(image))
+}
+
+function renderImage(image){
+  let imageTitle = document.getElementById('name')
+  imageTitle.innerHTML = `${image.name}`
+
+  let likes = document.getElementById('likes')
+  likes.innerText = `${image.like_count}`
+
+  // do below for like button
+  // likes.dataset.id = `${image.id}`
+
+
+  let imageCard = document.getElementById('image-card')
+  // imageCard.children[0].setAttribute
+  // imageCard.dataset.id = `${image.id}`
+  let setImage = document.createAttribute('IMG')
+  // img.innerHTML = `<img src='${image.url}'>`
+  setImage.setAttribute("src", `"${image.url}"`)
+  imageCard.append(setImage)
+
+  let comments = document.getElementById('comment-form')
+  let ul = document.createElement('ul')
+  
+  image.comments.forEach(comment=>{
+    `${comment.content}`
+  })
+  
+  comments.append(ul)
+
+
+  
+
+  // let newImage = document.getElementById('image')
+  // imageCard.innerHTML = `
+  // <h1>${image.name}</h1>
+  // <img src='${image.url}'>
+
+  // `
+  // imageCard.append(newImage)
+}
+
+// function addLikes(){
+
+// }
+
+document.addEventListener('click', function(e){
+  if(e.target.id === 'like_button'){
+    // let likeButton = document.getElementById('like_button')
+    // likeButton.dataset.id = image.id
+    num = e.target.parentElement.querySelector('') 
+    num++
+  }
+ 
+
+})
+
+////////
 })
