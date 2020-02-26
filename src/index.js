4d8e686ac9dc32d31d99de86ac840cd81cc1088e
Natalia Wit <natwit95@gmail.com>
2020-02-26 10:47:31 -0500

    deliverables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4610 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-})
+
+  fetch(imageURL)
+  .then((response) => {
+    return response.json();
+  })
+  .then((body) => {
+    body.array.forEach((image => {
+      let imageCard = document.getElementById("image_card")
+      let title = imageCard.querySelector("#name").innerText = "`${image.name}`"
+      imageCard.append(title)
+    };
+  });
+  
+
+})//dom
